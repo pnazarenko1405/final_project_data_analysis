32aad9fa735acfef4c8e4b610ee2be8fe776a9c2
polinanazarenko <pnazarenko@nes.ru>
2021-06-11 20:40:15 +0300

    in cmt

--- demo_app.py
+++ demo_app.py
@@ -26,6 +26,7 @@ import sqlite3
 with st.echo(code_location='below'):
 
     df1 = pd.read_csv('2019-20-fullyr-data_sa_crime.csv')
+    df2 = pd.read_csv("2018-19-data_sa_crime.csv")
 
 
     st.title("Различные данные по правонарушениям в Южной Австралии за 2018-2020гг.")
@@ -44,9 +45,9 @@ with st.echo(code_location='below'):
     plt.title('Total offence count of crimes in the suburbs (top 15) 2019/2020', size=36)
     st.pyplot(fig2)
     if st.button('Показать статистику по пригородам с наибольшим количествам правонарушений за 2019-2020гг. в виде таблицы'):
-        st.dataframe(group1)
+        st.table(data=group1)
 
-    xx1 = pd.read_csv('2019-20-fullyr-data_sa_crime.csv')
+    xx1 = df1.copy()
     xx1.drop(columns=['Reported Date', 'Postcode - Incident', 'Suburb - Incident', 'Offence Level 2 Description', 'Offence Level 3 Description'])
     xx1.sort_values(by='Offence Level 1 Description', ascending=False)
     groups1 = xx1.groupby('Offence Level 1 Description', as_index=False).sum()
@@ -60,25 +61,25 @@ with st.echo(code_location='below'):
     plt.title('Total offence count of different crimes (lev1) 2019/2020', size=36)
     st.pyplot(fig3)
     if st.button('Показать статистику по количеству правонарушений по первой классификации за 2019-2020гг. в виде таблицы'):
-        st.dataframe(group12)
+        st.table(data=group12)
 
-    xx2 = pd.read_csv('2019-20-fullyr-data_sa_crime.csv')
+    xx2 = df1.copy()
     xx2.drop(columns=['Reported Date', 'Postcode - Incident', 'Suburb - Incident', 'Offence Level 1 Description', 'Offence Level 3 Description'])
     xx2.sort_values(by='Offence Level 2 Description', ascending=False)
     groups1_2 = xx2.groupby('Offence Level 2 Description', as_index=False).sum()
     group123 = groups1_2.sort_values('Offence count', ascending=False)
 
-    st.write('Статистика по количеству правонарушений по второй классификации за 2019-2020гг.')
-    fig4, ax4 = plt.subplots(figsize=(40, 20))
-    sns.barplot(data=group123, x='Offence Level 2 Description', y='Offence count', palette='magma')
-    plt.xlabel('Type of crime (lev2)', size=20)
-    plt.ylabel('Offence count', size=20)
-    plt.title('Total offence count of different crimes (lev2) 2019/2020', size=36)
-    st.pyplot(fig4)
-    if st.button('Показать статистику по количеству правонарушений по второй классификации за 2019-2020гг. в виде таблицы'):
-        st.dataframe(group123)
+    # st.write('Статистика по количеству правонарушений по второй классификации за 2019-2020гг.')
+    # fig4, ax4 = plt.subplots(figsize=(40, 20))
+    # sns.barplot(data=group123, x='Offence Level 2 Description', y='Offence count', palette='magma')
+    # plt.xlabel('Type of crime (lev2)', size=20)
+    # plt.ylabel('Offence count', size=20)
+    # plt.title('Total offence count of different crimes (lev2) 2019/2020', size=36)
+    # st.pyplot(fig4)
+    # if st.button('Показать статистику по количеству правонарушений по второй классификации за 2019-2020гг. в виде таблицы'):
+    #     st.dataframe(group123)
 
-    xx3 = pd.read_csv('2019-20-fullyr-data_sa_crime.csv')
+    xx3 = df1.copy()
     xx3.drop(columns=['Reported Date', 'Postcode - Incident', 'Suburb - Incident', 'Offence Level 1 Description', 'Offence Level 2 Description'])
     xx3.sort_values(by='Offence Level 3 Description', ascending=False)
     groups1_2_3 = xx3.groupby('Offence Level 3 Description', as_index=False).sum()
@@ -92,9 +93,9 @@ with st.echo(code_location='below'):
     plt.title('Total offence count of different crimes (lev3)', size=36)
     st.pyplot(fig5)
     if st.button('Показать cтатистику по количеству правонарушений по третьей классификации за 2019-2020гг. в виде таблицы'):
-        st.dataframe(group1234)
+        st.table(data=group1234)
 
-    xx4 = pd.read_csv('2019-20-fullyr-data_sa_crime.csv')
+    xx4 = df1.copy()
     xx4.drop(columns=['Postcode - Incident', 'Suburb - Incident', 'Offence Level 1 Description', 'Offence Level 2 Description', 'Offence Level 3 Description'])
     xx4.sort_values(by='Reported Date')
     groups1_2_3_4 = xx4.groupby('Reported Date', as_index=False).sum()
@@ -108,10 +109,10 @@ with st.echo(code_location='below'):
     plt.title('Total offence count by date 01.07.19-30.06.20', size=36)
     st.pyplot(fig6)
     if st.button('Показать статистику по количеству правонарушений по датам за 2019-2020гг. в виде таблицы'):
-        st.dataframe(group12345)
+        st.table(data=group12345)
 
 
-    x_18_19=pd.read_csv ('2018-19-data_sa_crime.csv')
+    x_18_19 = df2.copy()
     x_18_19.drop(columns=['Reported Date', 'Postcode - Incident', 'Offence Level 1 Description', 'Offence Level 2 Description', 'Offence Level 3 Description'])
     x_18_19.sort_values(by='Suburb - Incident', ascending=False)
 
@@ -126,92 +127,92 @@ with st.echo(code_location='below'):
     plt.title('Total offence count of crimes in the suburbs (top 15) 2018/2019', size=36)
     st.pyplot(fig7)
     if st.button('Показать статистику по пригородам с наибольшим количествам правонарушений за 2018-2019гг. в виде таблицы'):
-        st.dataframe(group_18_19_1)
-    #
-    # x_18_19_2 = pd.read_csv('2018-19-data_sa_crime.csv')
-    # x_18_19_2.drop(columns=['Reported Date', 'Postcode - Incident', 'Suburb - Incident', 'Offence Level 2 Description', 'Offence Level 3 Description'])
-    # x_18_19_2.sort_values(by='Offence Level 1 Description', ascending=False)
-    # groups_18_19_2 = x_18_19_2.groupby('Offence Level 1 Description', as_index=False).sum()
-    # group_18_19_2 = groups_18_19_2.sort_values('Offence count', ascending=False)
-    #
-    # st.write('Статистика по количеству правонарушений по первой классификации за 2018-2019гг.')
-    # fig8, ax8 = plt.subplots(figsize=(40, 20))
-    # sns.barplot(data=group_18_19_2, x='Offence Level 1 Description', y='Offence count', palette='magma')
-    # plt.xlabel('Type of crime (lev1)', size=20)
-    # plt.ylabel('Offence count', size=20)
-    # plt.title('Total offence count of different crimes (lev1) 2018/2019', size=36)
-    # st.pyplot(fig8)
-    # if st.button('Показать статистику по количеству правонарушений по первой классификации за 2018-2019гг. в виде таблицы'):
-    #     st.dataframe(group_18_19_2)
-    #
-    # x_18_19_4 = pd.read_csv('2018-19-data_sa_crime.csv')
-    # x_18_19_4.drop(columns=['Reported Date', 'Postcode - Incident', 'Suburb - Incident', 'Offence Level 1 Description', 'Offence Level 3 Description'])
-    # x_18_19_4.sort_values(by='Offence Level 2 Description', ascending=False)
-    # groups_18_19_4 = x_18_19_4.groupby('Offence Level 2 Description', as_index=False).sum()
-    # group_18_19_4 = groups_18_19_4.sort_values('Offence count', ascending=False)
-    #
-    # st.write('Статистика по количеству правонарушений по второй классификации за 2018-2019гг.')
-    # fig10, ax10 = plt.subplots(figsize=(40, 20))
-    # sns.barplot(data=group_18_19_4, x='Offence Level 2 Description', y='Offence count', palette='magma')
-    # plt.xlabel('Type of crime (lev2)', size=20)
-    # plt.ylabel('Offence count', size=20)
-    # plt.title('Total offence count of different crimes (lev2) 2018/2019', size=36)
-    # st.pyplot(fig10)
-    # if st.button('Показать статистику по количеству правонврушений по второй классификации за 2018-2019гг. в виде таблицы'):
-    #     st.dataframe(group_18_19_4)
-    #
-    # x_18_19_3 = pd.read_csv('2018-19-data_sa_crime.csv')
-    # x_18_19_3.drop(columns=['Reported Date', 'Postcode - Incident', 'Suburb - Incident', 'Offence Level 1 Description', 'Offence Level 2 Description'])
-    # x_18_19_3.sort_values(by='Offence Level 3 Description', ascending=False)
-    # groups_18_19_3 = x_18_19_3.groupby('Offence Level 3 Description', as_index=False).sum()
-    # group_18_19_3 = groups_18_19_3.sort_values('Offence count', ascending=False)
-    #
-    # st.write('Статистика по количеству правонарушений по третьей классификации за 2018-2019гг.')
-    # fig9, ax9 = plt.subplots(figsize=(60, 20))
-    # sns.barplot(data=group_18_19_3, x='Offence Level 3 Description', y='Offence count', palette='magma')
-    # plt.xlabel('Type of crime (lev3)', size=20)
-    # plt.ylabel('Offence count', size=20)
-    # plt.title('Total offence count of different crimes (lev3) 2018/2019', size=36)
-    # st.pyplot(fig9)
-    # if st.button('Показать статистику по количеству правонарушений по третьей классификации за 2018-2019гг. в виде таблицы'):
-    #     st.dataframe(group_18_19_3)
+        st.table(data=group_18_19_1)
 
+    x_18_19_2 = df2.copy()
+    x_18_19_2.drop(columns=['Reported Date', 'Postcode - Incident', 'Suburb - Incident', 'Offence Level 2 Description', 'Offence Level 3 Description'])
+    x_18_19_2.sort_values(by='Offence Level 1 Description', ascending=False)
+    groups_18_19_2 = x_18_19_2.groupby('Offence Level 1 Description', as_index=False).sum()
+    group_18_19_2 = groups_18_19_2.sort_values('Offence count', ascending=False)
 
-    din=pd.read_csv("Offenders, principal offence of public order offences.csv")
-    #din_data = genfromtxt('Offenders, principal offence of public order offences.csv', delimiter=',')
-    print(din)
-    #din.columns=["Years", 'Offenders']
-    #print(din)
-    st.write('Статистика по количеству правонарушителей 2009-2019гг.')
+    st.write('Статистика по количеству правонарушений по первой классификации за 2018-2019гг.')
+    fig8, ax8 = plt.subplots(figsize=(40, 20))
+    sns.barplot(data=group_18_19_2, x='Offence Level 1 Description', y='Offence count', palette='magma')
+    plt.xlabel('Type of crime (lev1)', size=20)
+    plt.ylabel('Offence count', size=20)
+    plt.title('Total offence count of different crimes (lev1) 2018/2019', size=36)
+    st.pyplot(fig8)
+    if st.button('Показать статистику по количеству правонарушений по первой классификации за 2018-2019гг. в виде таблицы'):
+        st.table(data=group_18_19_2)
+
+    x_18_19_4 = df2.copy()
+    x_18_19_4.drop(columns=['Reported Date', 'Postcode - Incident', 'Suburb - Incident', 'Offence Level 1 Description', 'Offence Level 3 Description'])
+    x_18_19_4.sort_values(by='Offence Level 2 Description', ascending=False)
+    groups_18_19_4 = x_18_19_4.groupby('Offence Level 2 Description', as_index=False).sum()
+    group_18_19_4 = groups_18_19_4.sort_values('Offence count', ascending=False)
+
+    st.write('Статистика по количеству правонарушений по второй классификации за 2018-2019гг.')
     fig10, ax10 = plt.subplots(figsize=(40, 20))
-    sns.lineplot(data=din, x="Years", y='Offenders', color='red')
-    plt.xlabel('Year', size=40)
-    plt.ylabel('Offenders', size=40)
-    plt.title('Offenders dinamics', size=50)
+    sns.barplot(data=group_18_19_4, x='Offence Level 2 Description', y='Offence count', palette='magma')
+    plt.xlabel('Type of crime (lev2)', size=20)
+    plt.ylabel('Offence count', size=20)
+    plt.title('Total offence count of different crimes (lev2) 2018/2019', size=36)
     st.pyplot(fig10)
+    if st.button('Показать статистику по количеству правонврушений по второй классификации за 2018-2019гг. в виде таблицы'):
+        st.table(data=group_18_19_4)
 
-    if st.button('Показать статистику по количеству правонарушителей 2009-2019гг. в виде таблицы'):
-        st.dataframe(din)
+    x_18_19_3 = df2.copy()
+    x_18_19_3.drop(columns=['Reported Date', 'Postcode - Incident', 'Suburb - Incident', 'Offence Level 1 Description', 'Offence Level 2 Description'])
+    x_18_19_3.sort_values(by='Offence Level 3 Description', ascending=False)
+    groups_18_19_3 = x_18_19_3.groupby('Offence Level 3 Description', as_index=False).sum()
+    group_18_19_3 = groups_18_19_3.sort_values('Offence count', ascending=False)
 
-    years = np.array([2019, 2020])
-    st.write("(Придётся немного подождать, программа обрабатывает примерно 95тыс. результатов для каждого года)")
-
-    files = ['2019-20-fullyr-data_sa_crime.csv',
-             '2018-19-data_sa_crime.csv']  # выполняем весь этот код по созданию карты с маркерами для каждого файла
-    for file in files:
-
-
-        locations = []
-
-        entrypoint1 = "https://nominatim.openstreetmap.org/search"
-        query1 = {'q': 'MORPHETT VALE australia', 'format': 'xml'}
-        r1 = requests.get(entrypoint1, params=query1)
-        soup = BeautifulSoup(r1.text, 'xml')
+    st.write('Статистика по количеству правонарушений по третьей классификации за 2018-2019гг.')
+    fig9, ax9 = plt.subplots(figsize=(60, 20))
+    sns.barplot(data=group_18_19_3, x='Offence Level 3 Description', y='Offence count', palette='magma')
+    plt.xlabel('Type of crime (lev3)', size=20)
+    plt.ylabel('Offence count', size=20)
+    plt.title('Total offence count of different crimes (lev3) 2018/2019', size=36)
+    st.pyplot(fig9)
+    if st.button('Показать статистику по количеству правонарушений по третьей классификации за 2018-2019гг. в виде таблицы'):
+        st.table(data=group_18_19_3)
 
 
-        st.write("Визуализация количества правонарушений по пригородам на карте " + str(years[0]) + "-" + str(years[1]) + "гг.")
-        years = years-1
-        print(years)
+    # din=pd.read_csv("Offenders, principal offence of public order offences.csv")
+    # #din_data = genfromtxt('Offenders, principal offence of public order offences.csv', delimiter=',')
+    # print(din)
+    # #din.columns=["Years", 'Offenders']
+    # #print(din)
+    # st.write('Статистика по количеству правонарушителей 2009-2019гг.')
+    # fig10, ax10 = plt.subplots(figsize=(40, 20))
+    # sns.lineplot(data=din, x="Years", y='Offenders', color='red')
+    # plt.xlabel('Year', size=40)
+    # plt.ylabel('Offenders', size=40)
+    # plt.title('Offenders dinamics', size=50)
+    # st.pyplot(fig10)
+    #
+    # if st.button('Показать статистику по количеству правонарушителей 2009-2019гг. в виде таблицы'):
+    #     st.dataframe(din)
+    # #
+    # years = np.array([2019, 2020])
+    # st.write("(Придётся немного подождать, программа обрабатывает примерно 95тыс. результатов для каждого года)")
+    #
+    # files = ['2019-20-fullyr-data_sa_crime.csv',
+    #          '2018-19-data_sa_crime.csv']  # выполняем весь этот код по созданию карты с маркерами для каждого файла
+    # for file in files:
+    #
+    #
+    #     locations = []
+    #
+    #     entrypoint1 = "https://nominatim.openstreetmap.org/search"
+    #     query1 = {'q': 'MORPHETT VALE australia', 'format': 'xml'}
+    #     r1 = requests.get(entrypoint1, params=query1)
+    #     soup = BeautifulSoup(r1.text, 'xml')
+    #
+    #
+    #     st.write("Визуализация количества правонарушений по пригородам на карте " + str(years[0]) + "-" + str(years[1]) + "гг.")
+    #     years = years-1
+    #     print(years)
 
         #
         # with open(osp.join(os.environ['HOME'], 'Downloads/first_project 2', file), newline='') as f:  # если будут ошибки из-за пути, то просто вставь полный путь к папке с файлами csv
